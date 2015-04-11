a3d9fed2e38dca6f94a720770afd7dcf8f30a156
Romain Dalmais <Snayle@Avalon.local>
2015-04-11 19:28:28 +0200

    Add the rover start

---
+++ MarsLavaTubes/Assets/command_Center.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class command_Center : MonoBehaviour {
+
+	public GameObject mRoverInit;
+	private bool instanceInit;
+
+
+	// Use this for initialization
+	void Start () {
+		instanceInit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// On collision with something
+	void OnCollisionEnter(Collision collision) {
+/*
+		foreach (ContactPoint contact in collision.contacts) {
+			print ("Contact ");
+			if ((contact.Equals (GameObject.Find ("map"))) && 
+*/
+		if(instanceInit == false)
+		{
+			print ("Contact Mars !");
+			GameObject roverInit = Instantiate (mRoverInit);
+			instanceInit = true;
+		}
+	}
+}
---
+++ MarsLavaTubes/Assets/main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	public GameObject mTarget;
+	public float camSpeed = 10;
+	public float scrollSpeed = 500;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Upd ate is called once per frame
+	void Update () {
+		print ("tests caméra");
+		float forwardSpeed = Input.GetAxis ("Vertical") * Time.deltaTime * camSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * Time.deltaTime * camSpeed;
+		float zoomSpeed = Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * scrollSpeed;
+		transform.position -= new Vector3 (sideSpeed, forwardSpeed, zoomSpeed);
+	}
+
+
+}
