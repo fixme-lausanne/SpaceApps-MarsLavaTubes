1ec8e4801d162f701b58e0aab99ad7f086b551d8
Romain Dalmais <Snayle@Avalon.local>
2015-04-11 20:38:39 +0200

    move script

---
+++ MarsLavaTubes/Assets/Scripts/main.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	public GameObject mTarget;
+	public float camSpeed = 10;
+	public float scrollSpeed = 500;
+	public Collider lastObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Upd ate is called once per frame
+	void Update () {
+		float forwardSpeed = Input.GetAxis ("Vertical") * Time.deltaTime * camSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * Time.deltaTime * camSpeed;
+		float zoomSpeed = Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * scrollSpeed;
+		transform.position -= new Vector3 (sideSpeed, forwardSpeed, zoomSpeed);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+			hitInfo.collider.SendMessage("OnSelection()");
+			print ("Impact sur " +hitInfo.collider.name);
+			lastObject = hitInfo.collider;
+		} 
+	}
+
+
+
+}
---
+++ MarsLavaTubes/Assets/Scripts/sphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision toto){
+		print ("je suis la");
+	}
+
+}
