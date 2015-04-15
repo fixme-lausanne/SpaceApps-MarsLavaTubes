2ce8e3bee28811820f4f63e730a8871c2f4f97b6
Rorist <rorist@0xcafe.ch>
2015-04-16 00:22:28 +0200

    Better camera

--- MarsLavaTubes/Assets/Scripts/CameraControl.cs
+++ MarsLavaTubes/Assets/Scripts/CameraControl.cs
@@ -18,29 +18,78 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour
 {
-	public GameObject mMap;
+	public GameObject mBlocker;
 	float scrollSpeed;
 	float camSpeed;
 
 	void Start ()
 	{
-		scrollSpeed = 500;
 		camSpeed = 10;
 	}
 
 	void Update ()
 	{
+		float distanceToMap = 0;
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, Vector3.down, out hit)) {
+			distanceToMap = hit.distance;
+		}
 
 		// Camera position
 		float forwardSpeed = Input.GetAxis ("Vertical") * Time.deltaTime * camSpeed;
 		float sideSpeed = Input.GetAxis ("Horizontal") * Time.deltaTime * camSpeed;
-		float zoomSpeed = Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * scrollSpeed/2;
-		
-		if (Vector3.Distance (transform.position, mMap.transform.position) < 10f && zoomSpeed > 0) {
+		float zoomSpeed = Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * scrollSpeed;
+		if (distanceToMap <= 1f && zoomSpeed > 0) {
 			zoomSpeed = 0;
 		}
-
+		if (distanceToMap >= 64f && zoomSpeed < 0) {
+			zoomSpeed = 0;
+		}
 		transform.position += new Vector3 (forwardSpeed, -zoomSpeed, -sideSpeed);
+
+		// Camera rotation
+		int rotx;
+		if (distanceToMap > 40f) {
+			camSpeed = 80;
+			scrollSpeed = 4800;
+			rotx = 90;
+		} else if (distanceToMap > 35f) {
+			camSpeed = 70;
+			scrollSpeed = 3500;
+			rotx = 84;
+		} else if (distanceToMap > 30f) {
+			camSpeed = 60;
+			scrollSpeed = 2400;
+			rotx = 75;
+		} else if (distanceToMap > 25f) {
+			camSpeed = 50;
+			scrollSpeed = 1600;
+			rotx = 64;
+		} else if (distanceToMap > 20f) {
+			camSpeed = 40;
+			scrollSpeed = 1200;
+			rotx = 60;
+		} else if (distanceToMap > 15f) {
+			camSpeed = 30;
+			scrollSpeed = 900;
+			rotx = 54;
+		} else if (distanceToMap > 10f) {
+			camSpeed = 20;
+			scrollSpeed = 600;
+			rotx = 45;
+		} else if (distanceToMap > 5f) {
+			camSpeed = 10;
+			scrollSpeed = 300;
+			rotx = 34;
+		} else {
+			camSpeed = 5;
+			scrollSpeed = 100;
+			rotx = 30;
+		}
+		//transform.LookAt(new Vector3 (rotx, 90, 0));
+		transform.rotation = Quaternion.Euler (new Vector3 (rotx, 90, 0));
+
+
 	}
 
 	void OnTriggerEnter (Collider col)
