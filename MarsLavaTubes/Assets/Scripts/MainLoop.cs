4b64b7c5f13eb8e8bddb702f6ebd2bb999429497
Rorist <rorist@0xcafe.ch>
2015-04-15 15:44:28 +0200

    Random feature on map

--- MarsLavaTubes/Assets/Scripts/MainLoop.cs
+++ MarsLavaTubes/Assets/Scripts/MainLoop.cs
@@ -4,16 +4,28 @@ using System.Collections;
 public class MainLoop : MonoBehaviour
 {
 	
-	float camSpeed = 10;
-	float scrollSpeed = 500;
+	int terrainSize;
+	int featureNumber;
+	float scrollSpeed;
+	float camSpeed;
 	public GameObject mCamera;
 	public GameObject mRoverInit;
 	public GameObject mTarget;
 	public GameObject mSpawnPoint;
+	public GameObject mMapPoint;
+	public GameObject mCenter;
+
+	void Start ()
+	{
+		terrainSize = 15;
+		featureNumber = 30;
+		scrollSpeed = 500;
+		camSpeed = 10;
+		GeneratePoints ();
+	}
 
 	void Update ()
 	{
-
 		// Camera position
 		float forwardSpeed = Input.GetAxis ("Vertical") * Time.deltaTime * camSpeed;
 		float sideSpeed = Input.GetAxis ("Horizontal") * Time.deltaTime * camSpeed;
@@ -25,6 +37,17 @@ public class MainLoop : MonoBehaviour
 			GameObject rover = Instantiate (mRoverInit, mSpawnPoint.transform.position, mSpawnPoint.transform.rotation) as GameObject;
 			roverInit roverCmp = rover.GetComponent<roverInit> ();
 			roverCmp.target = mTarget;
+		}
+	}
+
+	void GeneratePoints ()
+	{
+		Vector2 randomPoint;
+		float randomRotation;
+		for (int i=0; i<featureNumber; i++) {
+			randomPoint = Random.insideUnitCircle * terrainSize;
+			randomRotation = Random.Range (0f, 180f);
+			Instantiate (mMapPoint, mCenter.transform.position + new Vector3 (randomPoint.x, 0, randomPoint.y), Quaternion.Euler (new Vector3 (-90, randomRotation, 0)));
 		}
 	}
 	
